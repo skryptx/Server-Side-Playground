7fdd8c0bc71b435e3c53c8ef23ab88871e063f09
singla-s <sinnisingla@outlook.com>
2023-10-16 00:24:07 +0530

    hashmap

---
+++ python/learn/hashmap.py
@@ -0,0 +1,12 @@
+""" This module contains implementation of hashmap aka dictionary in python"""
+hashmap = {}  # initializations
+
+hashmap["first_name"] = "sinni"
+hashmap["first_name"] = "sinni"
+print(hashmap)  # ans: {'first_name': 'sinni'}
+
+print(hashmap.values())  # ans: ["sinni"] returns list of values
+print(hashmap.keys())  # ans: ["first_name"]
+
+hashmap1 = dict.fromkeys(["first_name", "last_name"], ["sinni", "singla"])
+print(hashmap1)
