903537c68919bf4a62679e3a256f543f7a257815
singla-s <sinnisingla@outlook.com>
2023-10-15 20:12:10 +0530

    strings

--- python/learn/strings.py
+++ python/learn/strings.py
@@ -5,3 +5,9 @@ message = "Hello World's"
 print(len(message))
 
 print(message.find("o", 6))
+
+## strings are immutable
+# everytime a change is made to string, a new string is created
+# so string operations are O(n) time
+
+ message[0] = "W" #===>>> Not Allowed
