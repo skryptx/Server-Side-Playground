360bc524dc3ee8de08632c6e6279fa677df0e898
singla-s <sinnisingla@outlook.com>
2023-10-26 17:23:46 +0530

    complete assignment 1

---
+++ java/PrincetonUniversity/Algorithm-i/hello/week1/HelloWorld.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    StdOut.println("Hello, World");
+  }
+}
