5cfd8aeaf7e100eef53e42ff96227c499b93bde9
singla-s <sinnisingla@outlook.com>
2023-10-31 00:40:38 +0530

    java basics

---
+++ java/GroundWork/src/JavaBasics/PrimitiveTypes.java
@@ -0,0 +1,22 @@
+package JavaBasics;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        /*
+        Type                representation                  Range                           Bits
+        byte                b=5                             -128 - 127                      8
+        short               s=128                           -32768 - 32767                  16
+        int                 i=4000                          -2.14*10^9 - 2.14*10^9          32
+        long                l=22222222222                   -9.22*10^18 - 9.22*10^18        64
+        float               f=0.4f;
+        double              d=67.0
+        char                c='A'
+        boolean             isTrue=false
+         */
+
+        /*
+        Decimal and float are not very precise so not recommended for financials
+        Use BigDecimal instead
+         */
+    }
+}
---
+++ java/GroundWork/src/JavaBasics/Strings.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        /*
+        Left to right computation
+         */
+        System.out.println(2 + 3 + "4"); // 54
+        System.out.println("2" + 3 + 4); // 234
+
+        System.out.println('2' + 3 + 4); // 57 ? => Ascii equivalent of '2' is 50
+    }
+}
