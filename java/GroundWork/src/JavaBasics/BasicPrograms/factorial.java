2aac94c6a06c22773c0b356420a26cae91275ddc
singla-s <sinnisingla@outlook.com>
2023-11-12 00:52:48 +0530

    java: factorial

---
+++ java/GroundWork/src/JavaBasics/BasicPrograms/factorial.java
@@ -0,0 +1,19 @@
+package JavaBasics.BasicPrograms;
+
+public class factorial {
+    public static void main(String[] args) {
+        int res = calculateFactorial(-4);
+        System.out.println(res);
+    }
+
+    public static int calculateFactorial(int number) {
+        int prod = 1;
+        
+        if(number < 0) return -1;
+
+        while(number > 0) {
+            prod *= number--;
+        }
+        return prod;
+    }
+}
