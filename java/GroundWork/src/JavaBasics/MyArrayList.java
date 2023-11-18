41845b32f99261460116521c030bde9bbe824f2f
singla-s <sinnisingla@outlook.com>
2023-11-18 17:21:25 +0530

    java: array list

---
+++ java/GroundWork/src/JavaBasics/MyArrayList.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Sinni");
+        System.out.println(students);
+        System.out.println(students.size());
+    }
+}
