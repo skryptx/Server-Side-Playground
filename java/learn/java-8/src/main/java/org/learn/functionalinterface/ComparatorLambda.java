3311a667be33d6974599078fdd8c58eaa318bf6f
singla-s <sinnisingla@outlook.com>
2024-04-10 23:55:58 +0530

    feat: comparator lambda example

--- java/learn/java-8/src/main/java/org/learn/functionalinterface/ComparatorLambda.java
+++ java/learn/java-8/src/main/java/org/learn/functionalinterface/ComparatorLambda.java
@@ -1,11 +1,12 @@
 package org.learn.functionalinterface;
 import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComparatorLambda {
     public static void main(String[] args) {
         Comparator<String> comp  = Comparator.naturalOrder();
-        List<String> list = new java.util.ArrayList<>(List.of("Two", "One"));
+        List<String> list = new ArrayList<>(List.of("Two", "One"));
         list.sort(comp);
         list.forEach(System.out::println);
         System.out.println(list);
