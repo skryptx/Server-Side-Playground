eeb2bf5e34d2f4c98c47dcdfe3376bdd900a035e
singla-s <sinnisingla@outlook.com>
2024-04-18 00:39:57 +0530

    feat: reduce

---
+++ java/learn/java-8/src/main/java/org/learn/concepts/streams/ReduceExample.java
@@ -0,0 +1,14 @@
+package org.learn.concepts.streams;
+
+import java.util.List;
+
+public class ReduceExample {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1,2,3,4);
+        var result = nums.stream().reduce((a,b)->a*b);
+        var resultWithIdentity = nums.stream().reduce(10,(a,b)->a*b);
+        result.ifPresent(integer -> System.out.printf("Without Identity: %d\n", integer));
+
+        System.out.printf("With Identity: %d", resultWithIdentity);
+    }
+}
