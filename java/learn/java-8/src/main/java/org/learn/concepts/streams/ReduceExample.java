2593457f523aaa23f1b0e62c86ac9d50d1f29f01
singla-s <sinnisingla@outlook.com>
2024-04-29 23:54:35 +0530

    feat: reduce student list to get highest gpa student

--- java/learn/java-8/src/main/java/org/learn/concepts/streams/ReduceExample.java
+++ java/learn/java-8/src/main/java/org/learn/concepts/streams/ReduceExample.java
@@ -20,5 +20,6 @@ public class ReduceExample {
     public static void main(String[] args) {
         var highestGpaStudent = getHighestGpaStudent(new StudentDatabase().getStudents());
         highestGpaStudent.ifPresent(System.out::println);
+        return;
     }
 }
