627c4fa744c8c677426450e607fa61956f57a840
sisingla <sisingla@deloitte.com>
2024-05-04 01:15:57 +0530

    feat: check runtime

---
+++ java/learn/java-8/src/main/java/org/learn/concepts/streams/ParallelStreams.java
@@ -0,0 +1,7 @@
+package org.learn.concepts.streams;
+
+public class ParallelStreams {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
