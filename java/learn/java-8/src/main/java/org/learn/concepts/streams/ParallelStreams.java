e7febdd3860fdc7ae7681daca90da66045c9889a
singla-s <sinnisingla@outlook.com>
2024-05-05 00:12:25 +0530

    feat: collect and box stream

--- java/learn/java-8/src/main/java/org/learn/concepts/streams/ParallelStreams.java
+++ java/learn/java-8/src/main/java/org/learn/concepts/streams/ParallelStreams.java
@@ -1,9 +1,9 @@
 package org.learn.concepts.streams;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ParallelStreams {
     public static void main(String[] args) {
-        var list = IntStream.rangeClosed(1, 10);
-        System.out.println(list);
+        System.out.println(IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList()));
     }
 }
