c76ae77bb10238f1497920132151bcc026bb8484
singla-s <sinnisingla@outlook.com>
2024-05-10 00:29:18 +0530

    feat: use optional operators

--- java/learn/java-8/src/main/java/org/learn/concepts/Test.java
+++ java/learn/java-8/src/main/java/org/learn/concepts/Test.java
@@ -1,5 +1,8 @@
 package org.learn.concepts;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Test {
 
     public static int add(int... args) {
@@ -13,5 +16,10 @@ public class Test {
     public static void main(String[] args) {
         int[] nums = new int[]{1,2,3,4};
         System.out.println(add(nums));
+
+        List<Integer> numsList = Arrays.asList(1,2,3);
+        numsList.set(1,5);
+        numsList.sort(Integer::compareTo);
+        System.out.println(numsList);
     }
 }
