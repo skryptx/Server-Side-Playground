05b15e68593b0ca00aa4a0e04141280004becfca
singla-s <sinnisingla@outlook.com>
2024-05-10 23:55:39 +0530

    feat: cleanup

--- java/learn/java-8/src/main/java/org/learn/concepts/Test.java
+++ java/learn/java-8/src/main/java/org/learn/concepts/Test.java
@@ -1,17 +1,6 @@
 package org.learn.concepts;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Test {
-
-    public static int add(int... args) {
-        int sum = 0;
-        for(int i: args) {
-            sum+=i;
-        }
-        return sum;
-    }
 
     public static void main(String[] args) {
     }
