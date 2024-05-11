54b30599ae8bdedffaca94bb6faf2164eea40e4c
singla-s <sinnisingla@outlook.com>
2024-05-11 23:54:19 +0530

    feat: test date api

--- java/learn/java-8/src/main/java/org/learn/concepts/Test.java
+++ java/learn/java-8/src/main/java/org/learn/concepts/Test.java
@@ -1,7 +1,12 @@
 package org.learn.concepts;
 
+import java.time.LocalDate;
+
 public class Test {
 
     public static void main(String[] args) {
+        var month = LocalDate.now().getMonth();
+        System.out.println(month);
+        System.out.println(month.getValue());
     }
 }
