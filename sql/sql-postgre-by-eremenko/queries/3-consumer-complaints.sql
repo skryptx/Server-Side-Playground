8d51ec27b1392c7bcaef2126efd5efc35d478bbd
singla-s <sinnisingla@outlook.com>
2024-05-20 00:30:04 +0530

    feat: copy dataset into table

---
+++ sql/sql-postgre-by-eremenko/queries/3-consumer-complaints.sql
@@ -0,0 +1,3 @@
+COPY consumer_complaints FROM 'C:\Users\sinni\WorkSpace\dev-bytes-BE\sql\sql-postgre-by-eremenko\datasets\Section-3-ConsumerComplaints.csv' DELIMITER ',' CSV HEADER
+
+select * from consumer_complaints limit 1000
